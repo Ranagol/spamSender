9bf0d52e752c87af8f590c61d6f4ae792e6f011e
Andor <andorhorvat@gmail.com>
2020-05-19 16:55:01 +0200

    Routing issue solved

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -4,7 +4,7 @@
     <a class="nav-item nav-link" href="/emails/index">All contacts</a>
     <a class="nav-item nav-link" href="/add-email">Add contact</a>
     <a class="nav-item nav-link" href="#">Create MAIL</a>
-    <a class="nav-item nav-link" href="emailsending">MAIL sending</a>
+    <a class="nav-item nav-link" href="/emailsending">MAIL sending</a>
     <a class="nav-item nav-link" href="/backup">Backup and restore</a>
   </div>
 </nav>
