073a6a6bb9ebe337d52f9065b8f449470ede244d
Andor <andorhorvat@gmail.com>
2020-05-19 18:29:05 +0200

    18:29

--- resources/views/emailTemplates/mailForCustomers.blade.php
+++ resources/views/emailTemplates/mailForCustomers.blade.php
@@ -1,5 +1,274 @@
-<h1>Hi dear Customer!</h1>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
 
-<p>You can buy a Makita tool here: <a href="www.protechnic.rs">Awesome place to by Makita toos.</a></p>
-<p>Also here is a picture for you:</p>
-<img src="https://static01.nyt.com/images/2016/06/14/science/14weasels-web/14weasels-web-superJumbo.jpg" alt="weasel picture">
+<head>
+    <style class="darkreader darkreader--fallback" media="screen"></style>
+    <style class="darkreader darkreader--text" media="screen"></style>
+    <style class="darkreader darkreader--invert" media="screen">
+        .jfk-bubble,
+        embed[type="application/pdf"] {
+            filter: invert(100%) hue-rotate(180deg) contrast(90%) !important;
+        }
+    </style>
+    <style class="darkreader darkreader--inline" media="screen">
+        [data-darkreader-inline-bgcolor] {
+            background-color: var(--darkreader-inline-bgcolor) !important;
+        }
+
+        [data-darkreader-inline-bgimage] {
+            background-image: var(--darkreader-inline-bgimage) !important;
+        }
+
+        [data-darkreader-inline-border] {
+            border-color: var(--darkreader-inline-border) !important;
+        }
+
+        [data-darkreader-inline-border-bottom] {
+            border-bottom-color: var(--darkreader-inline-border-bottom) !important;
+        }
+
+        [data-darkreader-inline-border-left] {
+            border-left-color: var(--darkreader-inline-border-left) !important;
+        }
+
+        [data-darkreader-inline-border-right] {
+            border-right-color: var(--darkreader-inline-border-right) !important;
+        }
+
+        [data-darkreader-inline-border-top] {
+            border-top-color: var(--darkreader-inline-border-top) !important;
+        }
+
+        [data-darkreader-inline-boxshadow] {
+            box-shadow: var(--darkreader-inline-boxshadow) !important;
+        }
+
+        [data-darkreader-inline-color] {
+            color: var(--darkreader-inline-color) !important;
+        }
+
+        [data-darkreader-inline-fill] {
+            fill: var(--darkreader-inline-fill) !important;
+        }
+
+        [data-darkreader-inline-stroke] {
+            stroke: var(--darkreader-inline-stroke) !important;
+        }
+
+        [data-darkreader-inline-outline] {
+            outline-color: var(--darkreader-inline-outline) !important;
+        }
+    </style>
+    <style class="darkreader darkreader--user-agent" media="screen">
+        input,
+        textarea,
+        select,
+        button {
+            background-color: #181a1b;
+        }
+
+        html,
+        body,
+        input,
+        textarea,
+        select,
+        button {
+            border-color: #575757;
+            color: #e8e6e3;
+        }
+
+        a {
+            color: #3391ff;
+        }
+
+        table {
+            border-color: #4c4c4c;
+        }
+
+        ::placeholder {
+            color: #bab5ab;
+        }
+
+        ::selection {
+            background-color: #005ccc;
+            color: #ffffff;
+        }
+
+        ::-moz-selection {
+            background-color: #005ccc;
+            color: #ffffff;
+        }
+
+        input:-webkit-autofill,
+        textarea:-webkit-autofill,
+        select:-webkit-autofill {
+            background-color: #545b00 !important;
+            color: #e8e6e3 !important;
+        }
+
+        ::-webkit-scrollbar {
+            background-color: #1c1e1f;
+            color: #c5c1b9;
+        }
+
+        ::-webkit-scrollbar-thumb {
+            background-color: #2a2c2e;
+        }
+
+        ::-webkit-scrollbar-thumb:hover {
+            background-color: #323537;
+        }
+
+        ::-webkit-scrollbar-thumb:active {
+            background-color: #3d4043;
+        }
+
+        ::-webkit-scrollbar-corner {
+            background-color: #181a1b;
+        }
+
+        * {
+            scrollbar-color: #2a2c2e #1c1e1f;
+        }
+    </style>
+    <style class="darkreader darkreader--override" media="screen">
+        .vimvixen-hint {
+            background-color: #7b5300 !important;
+            border-color: #d8b013 !important;
+            color: #f3e8c8 !important;
+        }
+
+        ::placeholder {
+            opacity: 0.5 !important;
+        }
+    </style>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <meta name="x-apple-disable-message-reformatting">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="telephone=no" name="format-detection">
+    <title></title>
+    <!--[if (mso 16)]>
+    <style type="text/css">
+    a {text-decoration: none;}
+    </style>
+    <![endif]-->
+    <!--[if gte mso 9]><style>sup { font-size: 100% !important; }</style><![endif]-->
+    <style class="darkreader darkreader--sync" media="screen"></style>
+    <style class="darkreader darkreader--sync" media="screen"></style>
+    <style class="darkreader darkreader--sync" media="screen"></style>
+    <style class="darkreader darkreader--sync" media="screen"></style>
+    <style class="darkreader darkreader--sync" media="screen"></style>
+    <style class="darkreader darkreader--sync" media="screen"></style>
+    <style class="darkreader darkreader--sync" media="screen"></style>
+</head>
+
+<body>
+    <div class="es-wrapper-color">
+        <!--[if gte mso 9]>
+			<v:background xmlns:v="urn:schemas-microsoft-com:vml" fill="t">
+				<v:fill type="tile" color="#f6f6f6"></v:fill>
+			</v:background>
+		<![endif]-->
+        <table class="es-wrapper" width="100%" cellspacing="0" cellpadding="0">
+            <tbody>
+                <tr>
+                    <td class="esd-email-paddings" valign="top">
+                        <table class="esd-footer-popover es-content" cellspacing="0" cellpadding="0" align="center">
+                            <tbody>
+                                <tr>
+                                    <td class="esd-stripe" align="center">
+                                        <table class="es-content-body" width="600" cellspacing="0" cellpadding="0" bgcolor="#ffffff" align="center" data-darkreader-inline-bgcolor style="--darkreader-inline-bgcolor:#181a1b;">
+                                            <tbody>
+                                                <tr>
+                                                    <td class="esd-structure es-p20t es-p20b es-p20r es-p20l" align="left">
+                                                        <!--[if mso]><table width="560" cellpadding="0" 
+                        cellspacing="0"><tr><td width="270" valign="top"><![endif]-->
+                                                        <table class="es-left" cellspacing="0" cellpadding="0" align="left">
+                                                            <tbody>
+                                                                <tr>
+                                                                    <td class="es-m-p20b esd-container-frame" width="270" align="left">
+                                                                        <table width="100%" cellspacing="0" cellpadding="0">
+                                                                            <tbody>
+                                                                                <tr>
+                                                                                    <td align="center" class="esd-block-image" style="font-size: 0px;"><a target="_blank"><img class="adapt-img" src="https://hemnbl.stripocdn.email/content/guids/CABINET_96ca20f10f2f14cb6d0e64f2aa9683df/images/31281589902387619.JPG" alt style="display: block;" width="270"></a></td>
+                                                                                </tr>
+                                                                            </tbody>
+                                                                        </table>
+                                                                    </td>
+                                                                </tr>
+                                                            </tbody>
+                                                        </table>
+                                                        <!--[if mso]></td><td width="20"></td><td width="270" valign="top"><![endif]-->
+                                                        <table class="es-right" cellspacing="0" cellpadding="0" align="right">
+                                                            <tbody>
+                                                                <tr>
+                                                                    <td class="esd-container-frame" width="270" align="left">
+                                                                        <table width="100%" cellspacing="0" cellpadding="0">
+                                                                            <tbody>
+                                                                                <tr>
+                                                                                    <td align="center" class="esd-block-image" style="font-size: 0px;"><a target="_blank"><img class="adapt-img" src="https://hemnbl.stripocdn.email/content/guids/CABINET_96ca20f10f2f14cb6d0e64f2aa9683df/images/52041589902387199.jpg" alt style="display: block;"></a></td>
+                                                                                </tr>
+                                                                            </tbody>
+                                                                        </table>
+                                                                    </td>
+                                                                </tr>
+                                                            </tbody>
+                                                        </table>
+                                                        <!--[if mso]></td></tr></table><![endif]-->
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="esd-structure es-p20t es-p20r es-p20l" align="left">
+                                                        <table cellpadding="0" cellspacing="0" width="100%">
+                                                            <tbody>
+                                                                <tr>
+                                                                    <td width="560" class="esd-container-frame" align="center" valign="top">
+                                                                        <table cellpadding="0" cellspacing="0" width="100%">
+                                                                            <tbody>
+                                                                                <tr>
+                                                                                    <td align="center" class="esd-block-image" style="font-size: 0px;"><a target="_blank"><img class="adapt-img" src="https://hemnbl.stripocdn.email/content/guids/CABINET_96ca20f10f2f14cb6d0e64f2aa9683df/images/80351589902417141.png" alt style="display: block;" width="560"></a></td>
+                                                                                </tr>
+                                                                            </tbody>
+                                                                        </table>
+                                                                    </td>
+                                                                </tr>
+                                                            </tbody>
+                                                        </table>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="esd-structure es-p20t es-p20r es-p20l" align="left">
+                                                        <table cellpadding="0" cellspacing="0" width="100%">
+                                                            <tbody>
+                                                                <tr>
+                                                                    <td width="560" class="esd-container-frame" align="center" valign="top">
+                                                                        <table cellpadding="0" cellspacing="0" width="100%">
+                                                                            <tbody>
+                                                                                <tr>
+                                                                                    <td align="left" class="esd-block-text">
+                                                                                        <p>Ez itt a fejléc, tralala</p>
+                                                                                    </td>
+                                                                                </tr>
+                                                                            </tbody>
+                                                                        </table>
+                                                                    </td>
+                                                                </tr>
+                                                            </tbody>
+                                                        </table>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+
+</html>
